1a191b3eb2e83042a3bb1f6e6f42a67f97c2ebcd
Nicsena <Nicholassena8190@gmail.com>
2022-12-04 23:35:11 +0000

    Changes to files

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require("express");
 const app = express();
 const fs = require("fs");
@@ -5,11 +7,13 @@ const path = require("path");
 const port = process.env.PORT || "3000";
 const blacklistedfiles = require("./blacklisted.json");
 const verbose = process.env.VERBOSE || "false";
+const logrequests = process.env.LOGREQUESTS || "false"
+let requests = 1;
 
 const defaultPath = "/";
 
 
-// ---- Functions -----
+// =================== Functions ===================
 
 function formatBytes(bytes, decimals = 2) {
   if (bytes === 0) return "0 Bytes";
@@ -23,16 +27,18 @@ function formatBytes(bytes, decimals = 2) {
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
 }
 
-function getFiles(directory) {
+
+// ======================================================
+// This function returns a json array of files and directories
+// in a directory along with some information about them.
+//
+// Example:
+// getFiles("/")
+// ======================================================
+async function getFiles(directory) {
   const fullPath = path.join(defaultPath + path.resolve(directory) + "/");
 
-  console.log(fullPath);
-
-  //console.log(fullPath.substring(0, defaultPath.length) !== defaultPath)
-
-  // if(fullPath.substring(0, defaultPath.length) !== defaultPath) {
-  //    return getFiles("/")
-  //   }
+  if(verbose === "true") console.log(`Directory: ${fullPath}`);
 
   try {
     const items = fs.readdirSync(fullPath, { withFileTypes: true });
@@ -42,17 +48,16 @@ function getFiles(directory) {
     items.forEach((item) => {
       // Don't add files/directories that is in the blacklist.
       if (blacklistedfiles.includes(item.name)) {
-        if (item.isDirectory())
-          return console.log(`Blacklisted Folder: ${item.name}`);
+        if (item.isDirectory()) return console.log(`Blacklisted Folder: ${item.name}`);
         if (item.isFile()) return console.log(`Blacklisted File: ${item.name}`);
-        if (item.isSymbolicLink())
-          return console.log(`Blacklisted Symbolic Link: ${item.name}`);
+        if (item.isSymbolicLink()) return console.log(`Blacklisted Symbolic Link: ${item.name}`);
       }
 
       // Get Stats for and push them to a array.
       const iteminfo = fs.statSync(fullPath + item.name);
       const json = {
         name: item.name,
+        path: fullPath + item.name,
         attributes: {
           mode: (iteminfo.mode & parseInt("777", 8)).toString(8),
           mode_bits: iteminfo.mode,
@@ -69,6 +74,8 @@ function getFiles(directory) {
     });
 
     return { path: "/", list: list };
+
+
   } catch (error) {
     if (error.code === "ENOENT") {
       console.log(`ENOENT: Directory Not Found - Path: ${fullPath}`);
@@ -89,8 +96,18 @@ function getFiles(directory) {
   }
 }
 
-function GetFileContent(file) {
+
+
+// ======================================================
+// This function returns the content of a specified file.
+//
+// Example:
+// GetFileContent("/hello.txt")
+// ======================================================
+async function GetFileContent(file) {
   const fullPath = path.join(defaultPath, path.resolve(file));
+
+  if(verbose === "true") console.log(`File: ${fullPath}`)
 
   try {
     const content = fs.readFileSync(fullPath, "utf8");
@@ -115,27 +132,110 @@ function GetFileContent(file) {
   }
 }
 
-// ---- Express Web Server -----
+
+
+
+// ======================================================
+// This function writes to a specified file with 
+// specified content.
+//
+// Example:
+// writeContentToFile("/hello.txt", "Content of the file")
+// ======================================================
+async function writeContentToFile(file, content) {
+  const fullPath = path.join(defaultPath, path.resolve(file));
+
+  if(verbose === "true") console.log(`File: ${fullPath}\nContent: ${content}`)
+
+  try {
+    const file = fs.writeFileSync(fullPath, content);
+    return "";
+
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      console.log(`ENOENT: File Not Found - Path: ${fullPath}`);
+      return "ENOENT";
+    }
+
+    if (error.code === "EACCES") {
+      console.log(`EACCES: Permission denied - Path: ${fullPath}`);
+      return "EACCES";
+    }
+
+    if (error.code === "ENOTDIR") {
+      console.log(`ENOTDIR: Not a directory - Path: ${fullPath}`);
+      return "ENOTDIR";
+    }
+
+    console.error(error);
+  }
+}
+
+
+
+
+
+// =================== Express Web Server ===================
 
 app.use(express.static("public"));
 app.enable("trust proxy");
+
 
 app.use((req, res, next) => {
   res.set("Cache-Control", "no-store");
   next();
 });
 
+
+
+app.get("*", (req, res, next) => {
+
+  if(logrequests === "true") {
+  //const IP = req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'].split(",")[0] || req.ip;
+  const IP = req.ip
+  var time = new Date();
+  var timestring = time.toLocaleTimeString();
+  const UserAgent = `${req.get('User-Agent')}` || `No User Agent!`
+  const Path = req.path
+  const Params = JSON.stringify(req.query)
+  const Method = req.method
+  const Version = req.httpVersion
+  const Host = req.hostname
+  const Referer = req.get('referer') || "None";
+
+  console.log(`[${timestring}] [${requests++}] [Method: ${Method}] [HTTP ${Version}] [IP: ${IP}] [Path: ${Path}] [Params: ${Params}] [Referer: ${Referer}] [User Agent: ${UserAgent}]`); 
+
+  }
+
+  next();
+
+});
+
+
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+
+
+
+
+// ======================================================
+// Files Endpoint: /files
+// This returns JSON result with files and directories
+// of a directory.
+//
+// Example:
+// http://127.0.0.1:3000/files?directory=/
+// ======================================================
 app.get("/files", async (req, res) => {
-  const path = req.query.directory;
-  const list = getFiles(path);
+  const directory = path.join(req.query.directory);
+  const list = await getFiles(directory);
 
   if (list === "ENOENT") {
     return res.status(404).json({
-      path: path,
+      path: directory,
       code: 404,
       message: `Directory doesn't exist`
     });
@@ -143,7 +243,7 @@ app.get("/files", async (req, res) => {
 
   if (list === "EACCES") {
     return res.status(403).json({
-      path: path,
+      path: directory,
       code: 403,
       message: `Forbidden`
     });
@@ -151,7 +251,7 @@ app.get("/files", async (req, res) => {
 
   if (list === "ENOTDIR") {
     return res.status(404).json({
-      path: path,
+      path: directory,
       code: 404,
       message: `Error 404`
     });
@@ -160,6 +260,15 @@ app.get("/files", async (req, res) => {
   res.status(200).json(list);
 });
 
+
+
+// ======================================================
+// Files Endpoint: /files/content
+// This endpoint returns contents of a file.
+//
+// Example:
+// http://127.0.0.1:3000/files/content?file=/hello.txt
+// ======================================================
 app.get("/files/content", async (req, res) => {
   const file = req.query.file;
   const content = GetFileContent(file);
@@ -191,6 +300,10 @@ app.get("/files/content", async (req, res) => {
   res.header("Content-Type", "text/plain;charset=UTF-8");
   res.status(200).send(content);
 });
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
